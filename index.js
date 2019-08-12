94b05684263a48249d29bb2f9d208e45f7da932e
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-08-12 10:02:29 +0100

    refactored media streaming

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-var mqtt = require('mqtt');
-var hostname = "mqtt://raspberrypi.local";
-var client  = mqtt.connect(hostname);
-var express = require('express')
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var fs = require('fs')
-var say = require('say')
-var LifxClient = require('node-lifx').Client;
-var clientLight = new LifxClient();
+const mqtt = require('mqtt');
+const hostname = "mqtt://raspberrypi.local";
+const client  = mqtt.connect(hostname);
+const express = require('express')
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const fs = require('fs')
+const say = require('say')
+const LifxClient = require('node-lifx').Client;
+const clientLight = new LifxClient();
 
+// init new lightStrip to be used throughout script
 let lightStrip 
 clientLight.on('light-new', function(light){
   lightStrip = light
 })
-
 clientLight.init()
 
+// listen to app on port 3000
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
-
+//add HTML, public folder for stylesheets
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
   });
-
 app.use(express.static(__dirname + '/public'));
 
-
+// connect to snips
 client.on('connect', function () {
     console.log("[Snips Log] Connected to MQTT broker " + hostname);
     client.subscribe('hermes/#');
@@ -45,33 +45,32 @@ client.on('message', function (topic, message) {
     }
 });
 
+// send intent through rest of script
 function onIntentDetected(intent) {
     // console.log("[Snips Log] Intent detected: " + JSON.stringify(intent));
     getAction(intent)
 }
 
+// flash white lights when hotword is detected
 function onHotwordDetected() {
     console.log("[Snips Log] Hotword detected");
     lightStrip.colorRgb(255, 255, 255, 100)
     lightStrip.on()
     const lightStripOff = () => lightStrip.off()
     setTimeout(lightStripOff, 1500)
-    
 }
 
-function onListeningStateChanged(listening) {
-    
-    console.log("[Snips Log] " + (listening ? "Start" : "Stop") + " listening");
-
-
+function onListeningStateChanged(listening) {  
+  console.log("[Snips Log] " + (listening ? "Start" : "Stop") + " listening");
 }
 
+// connect to websocket
 io.on('connection', function(socket) {
     console.log('A user connected');
     console.log(clientLight.lights())
 });
 
-
+// take intent and send to frontend for media, process lights and audio feedback on server
 const getAction = intent => {
   console.log(intent)
   let action = intent.slots[0].slotName
@@ -95,21 +94,15 @@ const getAction = intent => {
     else if(action == 'off'){
       say.speak('Goodbye')
       lightStrip.off()
-    }  
-    
+    }     
 }
 
+// add all mp4s an loop over them to create routes
+const vidRoutes = ['tour', 'portfolio', 'history']
 
-
-
-
-
-
-
-
-
-app.get('/tour', function(req, res) {
-    const path = './videos/tour.mp4'
+vidRoutes.forEach(el => {
+  app.get(`/${el}`, function(req, res) {
+    const path = `./videos/${el}.mp4`
     const stat = fs.statSync(path)
     const fileSize = stat.size
     const range = req.headers.range
@@ -138,65 +131,4 @@ app.get('/tour', function(req, res) {
       fs.createReadStream(path).pipe(res)
     }
   });
-
-  app.get('/history', function(req, res) {
-    const path = './videos/history.mp4'
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] 
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
-  });
-
-  app.get('/portfolio', function(req, res) {
-    const path = './videos/portfolio.mp4'
-    const stat = fs.statSync(path)
-    const fileSize = stat.size
-    const range = req.headers.range
-    if (range) {
-      const parts = range.replace(/bytes=/, "").split("-")
-      const start = parseInt(parts[0], 10)
-      const end = parts[1] 
-        ? parseInt(parts[1], 10)
-        : fileSize-1
-      const chunksize = (end-start)+1
-      const file = fs.createReadStream(path, {start, end})
-      const head = {
-        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-        'Accept-Ranges': 'bytes',
-        'Content-Length': chunksize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(206, head);
-      file.pipe(res);
-    } else {
-      const head = {
-        'Content-Length': fileSize,
-        'Content-Type': 'video/mp4',
-      }
-      res.writeHead(200, head)
-      fs.createReadStream(path).pipe(res)
-    }
-  });
+})
